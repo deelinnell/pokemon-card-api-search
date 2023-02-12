febe13991c2d51dda6146b3cf6b4b07d17e1f6e4
dallind <61264144+dallind@users.noreply.github.com>
2023-02-12 14:36:07 -0700

    added all and subtype buttons

--- index.js
+++ index.js
@@ -1,69 +1,15 @@
+let allCards = []
 let currentArray = []
-let currentIndex = ''
+let currentModalIndex = ''
 let currentList = 0
+let loadedLists = {}
 let numberOfLists = 0
+let listLength = 18
+
+/*** SUBMIT REQUEST ***/
 
 const form = document.getElementById('input_form')
 const listContainer = document.getElementById('response_list_container')
-const seriesList = document.getElementById('series_list')
-const seriesTitle = document.getElementById('series_title')
-const hamberger = document.getElementById('hamberger')
-const filters = document.getElementById('filter_container')
-
-seriesList.style.display = 'none'
-seriesTitle.onclick = () => expandSort(seriesList, seriesTitle)
-
-function expandSort(list, title) {
-    if (list.style.display == 'none') {
-        openLists(list, title)
-        return
-    } else {
-        closeLists(list, title)
-    }
-}
-
-function openLists(list, title) {
-    list.style.display = 'block'
-    title.children[0].innerHTML = '-- '
-    title.classList.add('open')
-    expandFilters()
-}
-
-function closeLists(list, title) {
-    list.style.display = 'none'
-    title.children[0].innerHTML = '+ '
-    title.classList.remove('open')
-}
-
-hamberger.onclick = () => toggleFilters()
-
-function toggleFilters() {
-    if (hasClass(filters, 'hidden')) {
-        expandFilters()
-        return
-    }
-    colapseFilters()
-}
-
-function expandFilters() {
-    filters.classList.remove('hidden')
-}
-
-function colapseFilters() {
-    filters.classList.add('hidden')
-    const titles = document.querySelectorAll('.filter_title')
-    const lists = document.querySelectorAll('.filter_list')
-
-    for (let x = 0; x < titles.length; x++) {
-        closeLists(lists[x], titles[x])
-    }
-}
-
-function hasClass(element, clsName) {
-    return (' ' + element.className + ' ').indexOf(' ' + clsName + ' ') > -1;
-}
-
-/*** SUBMIT REQUEST ***/
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault()
@@ -75,8 +21,10 @@ form.addEventListener('submit', async function (e) {
 
     const data = await getRequest(request)
     const cards = data.cards
-    console.log(cards)
+    allCards = cards
+
     createSeriesList(cards)
+    createSubtypeList(cards)
     createAllLists(cards)
     openLists(seriesList, seriesTitle)
 
@@ -97,115 +45,81 @@ async function getRequest(url) {
 function createAllLists(array) {
     let ulID = 0
     currentList = 0
-    let listLength = 18
+    loadedLists = {}
     let listNumber = Math.floor(array.length / listLength)
     numberOfLists = listNumber
-    const lists = document.createElement('div')
+    const div = document.createElement('div')
 
     for (let x = 0; x < array.length; x += listLength) {
-        const ul = createUL(ulID)
-        array.slice(x, x + listLength).map((card) => {
-            createList(card, lists, ul)
-        })
+        const list = createList(ulID)
+        div.appendChild(list)
         ulID++
     }
 
     const remander = array.length % listLength
     if (remander && array.length > remander) {
-        const lastCards = array.slice(remander, array.length)
-        const ul = createUL(ulID)
+        const list = createList(ulID)
+        div.appendChild(list)
         numberOfLists++
-        lastCards.map(card => {
-            createList(card, lists, ul)
-        })
     }
 
-    listContainer.replaceChildren(lists)
+    listContainer.replaceChildren(div)
 
-    document.getElementById('list-0').style.display = 'grid'
-    currentArray = array
+    data = array
+    displayImages()
 }
 
-function createList(card, lists, list) {
-
-    const li = document.createElement('li')
-    const img = document.createElement('img')
-    img.src = card.imageUrl
-    img.onclick = () => cardOnclick(card)
-
-    li.appendChild(img)
-    list.appendChild(li)
-    lists.appendChild(list)
-}
-
-// function createList(card, lists, list) {
-
-//     const li = document.createElement('li')
-
-//     const button = document.createElement('button')
-//     button.innerHTML = 'Image'
-//     button.onclick = () => changeImage(card.imageUrl)
-
-//     const div = document.createElement('div')
-
-//     const h2 = document.createElement('h2')
-//     h2.innerHTML = card.name + ':'
-
-//     const h3 = document.createElement('h3')
-//     h3.innerHTML = card.set + ' | ' + card.series
-
-//     div.appendChild(h2)
-//     div.appendChild(h3)
-//     li.appendChild(div)
-//     li.appendChild(button)
-//     list.appendChild(li)
-//     lists.appendChild(list)
-// }
-
-function createUL(x) {
+function createList(ulID) {
     const ul = document.createElement('ul')
-    ul.setAttribute('id', 'list-' + x)
+    ul.setAttribute('id', 'list-' + ulID)
     ul.classList.add('list')
     return ul
 }
 
-function createSeriesList(array) {
-    const cards = array
-    const seriesList = {}
-    const list = document.getElementById('series_list')
-    list.replaceChildren('')
-
-    cards.map(card => {
-        const series = card.series
-        seriesList[card.series] = series
-    })
-
-    Object.values(seriesList).forEach(value => {
-        const li = document.createElement('li')
-        li.innerHTML = value
-        li.onclick = () => onclickFilter(value, cards)
-        list.appendChild(li)
-    })
+function addCardsToList(lists, list) {
+    lists.appendChild(list)
 }
 
-function onclickFilter(value, array) {
-    const filtered = array.filter(card => {
-        if (card.series == value) {
-            return card
-        }
-    })
-    createAllLists(filtered)
+/**** CARD FUNCTIONS ****/
+
+function displayImages() {
+    const listId = 'list-' + currentList
+    const list = document.getElementById(listId)
+    const start = currentList * listLength
+    const end = (currentList + 1) * listLength
+
+    currentArray = data.slice(start, end)
+    currentArray.map(card => createCard(card, list))
+    list.style.display = 'grid'
+    loadedLists[listId] = true
 }
 
-const modal = document.getElementById('image_modal')
-const imageContainer = document.getElementById('image_container')
-
-const rightContainer = document.querySelector('.right_container')
+function createCard(card, list) {
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    img.src = card.imageUrl
+    img.onclick = () => cardOnclick(card)
+    li.appendChild(img)
+    list.appendChild(li)
+}
 
 function cardOnclick(card) {
-    const imageArray = currentArray.slice(currentList * 18, (currentList + 1) * 18)
-    currentIndex = imageArray.indexOf(card)
+    currentModalIndex = currentArray.indexOf(card)
+    console.log(card)
 
+    const container = createCardInfo(card)
+    const container2 = createCardInfo(card)
+
+    modalCardInfo.replaceChildren(container)
+    rightContainer.insertBefore(container2, rightContainer.children[0])
+
+    changeImage(card.imageUrlHiRes)
+    modalButtons()
+
+    modal.style.display = 'flex'
+}
+
+function createCardInfo(card) {
     const container = document.createElement('div')
     const div2 = document.createElement('div')
     const div3 = document.createElement('div')
@@ -233,32 +147,177 @@ function cardOnclick(card) {
     div3.appendChild(seriesName)
     container.appendChild(div3)
 
-    rightContainer.insertBefore(container, rightContainer.children[0])
-
-    changeImage(card.imageUrlHiRes)
-    modalButtons(imageArray)
-
-    modal.style.display = 'flex'
+    return container
 }
 
-function modalButtons(array) {
+/**** FILTER TOGGLES ****/
+
+const seriesList = document.getElementById('series_list')
+const subtypeList = document.getElementById('subtype_list')
+const seriesTitle = document.getElementById('series_title')
+const subtypeTitle = document.getElementById('subtype_title')
+const hamberger = document.getElementById('hamberger')
+const filters = document.getElementById('filter_container')
+const allButton = document.getElementById('all_button')
+const allChains = document.getElementById('all_chains')
+const lists = document.querySelectorAll('.filter_list')
+const titles = document.querySelectorAll('.filter_title')
+
+seriesTitle.onclick = () => expandSort(seriesList, seriesTitle)
+subtypeTitle.onclick = () => expandSort(subtypeList, subtypeTitle)
+
+function expandSort(list, title) {
+    if (hasClass(list, 'open')) {
+        closeLists(list, title)
+        return
+    } else {
+        console.log(lists)
+        lists.forEach((li, index) => closeLists(li, titles[index]))
+        openLists(list, title)
+    }
+}
+
+function openLists(list, title) {
+    list.classList.add('open')
+    title.children[0].innerHTML = '-- '
+    title.classList.add('open')
+    expandFilters()
+}
+
+function closeLists(list, title) {
+    list.classList.remove('open')
+    title.children[0].innerHTML = '+ '
+    title.classList.remove('open')
+}
+
+hamberger.onclick = () => toggleFilters()
+
+function toggleFilters() {
+    if (hasClass(filters, 'hidden')) {
+        expandFilters()
+        return
+    }
+    collapseFilters()
+}
+
+function expandFilters() {
+    filters.classList.remove('hidden')
+    setTimeout(() => {
+        allButton.classList.remove('hidden')
+        allChains.classList.remove('hidden')
+    }, 200)
+}
+
+function collapseFilters() {
+    filters.classList.add('hidden')
+    allButton.classList.add('hidden')
+    allChains.classList.add('hidden')
+    const titles = document.querySelectorAll('.filter_title')
+    const lists = document.querySelectorAll('.filter_list')
+
+    for (let x = 0; x < titles.length; x++) {
+        closeLists(lists[x], titles[x])
+    }
+}
+
+function hasClass(element, clsName) {
+    return (' ' + element.className + ' ').indexOf(' ' + clsName + ' ') > -1;
+}
+
+// **** FILTER FUNCTIONS ****
+
+allButton.onclick = () => createAllLists(allCards)
+
+function createSeriesList(cards) {
+    let seriesList = {}
+    const list = document.getElementById('series_list')
+    list.replaceChildren('')
+
+    cards.map(card => {
+        const series = card.series
+        seriesList[card.series] = series
+    })
+
+    Object.values(seriesList).forEach(value => {
+        const li = document.createElement('li')
+        li.innerHTML = value
+        li.onclick = () => onclickSeries(value, cards)
+        list.appendChild(li)
+    })
+}
+
+function onclickSeries(value, cards) {
+    const filtered = cards.filter(card => {
+        if (card.series == value) {
+            return card
+        }
+    })
+    createAllLists(filtered)
+}
+
+function createSubtypeList(cards) {
+    let subtypeList = {}
+    const list = document.getElementById('subtype_list')
+    list.replaceChildren('')
+
+    cards.map(card => {
+        const subtype = card.subtype
+        subtypeList[card.subtype] = subtype
+    })
+
+    Object.values(subtypeList).forEach(value => {
+        const li = document.createElement('li')
+        li.innerHTML = value
+        li.onclick = () => onclickSubtype(value, cards)
+        list.appendChild(li)
+    })
+}
+
+function onclickSubtype(value, cards) {
+    const filtered = cards.filter(card => {
+        if (card.subtype == value) {
+            return card
+        }
+    })
+    createAllLists(filtered)
+}
+
+/*** MODAL ***/
+
+const modal = document.getElementById('image_modal')
+const imageContainer = document.getElementById('image_container')
+const modalCardInfo = document.getElementById('model_card_info')
+
+const rightContainer = document.querySelector('.right_container')
+
+function modalButtons() {
     const left = document.getElementById('modal_left')
     const right = document.getElementById('modal_right')
     left.onclick = () => {
-        if (currentIndex === 0) {
-            currentIndex = array.length - 1
+        if (currentModalIndex === 0) {
+            currentModalIndex = currentArray.length - 1
         } else {
-            currentIndex--
+            currentModalIndex--
         }
-        changeImage(array[currentIndex].imageUrlHiRes)
+        const card = currentArray[currentModalIndex]
+        changeImage(card.imageUrlHiRes)
+        const container = createCardInfo(card)
+        const container2 = createCardInfo(card)
+        modalCardInfo.replaceChildren(container)
+        rightContainer.insertBefore(container2, rightContainer.children[0])
     }
     right.onclick = () => {
-        if (currentIndex === array.length - 1) {
-            currentIndex = 0
+        if (currentModalIndex === currentArray.length - 1) {
+            currentModalIndex = 0
         } else {
-            currentIndex++
+            currentModalIndex++
         }
-        changeImage(array[currentIndex].imageUrlHiRes)
+        const card = currentArray[currentModalIndex]
+        changeImage(card.imageUrlHiRes)
+        const container = createCardInfo(card)
+        const container2 = createCardInfo(card)
+        modalCardInfo.replaceChildren(container)
+        rightContainer.insertBefore(container2, rightContainer.children[0])
     }
 }
 
@@ -274,29 +333,24 @@ window.onclick = function (event) {
     }
 }
 
-document.getElementById('left_button').onclick = () => {
-    if (currentList > 0) moveListsLeft()
-}
-document.getElementById('right_button').onclick = () => {
-    if (currentList < numberOfLists - 1) moveListsRight()
-}
+/** LIST BUTTONS **/
 
-function moveListsLeft() {
+document.getElementById('left_button').onclick = () => (currentList > 0) ? changeList(currentList - 1) : false
+document.getElementById('right_button').onclick = () => (currentList < numberOfLists - 1) ? changeList(currentList + 1) : false
+
+function changeList(listNum) {
     const oldList = document.getElementById('list-' + currentList)
     oldList.style.display = 'none'
 
-    currentList = currentList - 1
+    currentList = listNum
 
-    const newList = document.getElementById('list-' + currentList)
-    newList.style.display = 'grid'
-}
-
-function moveListsRight() {
-    const oldList = document.getElementById('list-' + currentList)
-    oldList.style.display = 'none'
-
-    currentList = currentList + 1
-
-    const newList = document.getElementById('list-' + currentList)
-    newList.style.display = 'grid'
+    const newListId = 'list-' + currentList
+    if (!loadedLists[newListId]) {
+        displayImages()
+    } else {
+        document.getElementById(newListId).style.display = 'grid'
+        const start = currentList * listLength
+        const end = (currentList + 1) * listLength
+        currentArray = data.slice(start, end)
+    }
 }
